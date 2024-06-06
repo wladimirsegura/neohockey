a42a286ec4b57613e98e77fd03938bab066f6109
wladimirsegura <rebeldes96@gmail.com>
2024-06-07 03:35:29 +0900

    added `vercel Analytics`

--- app/layout.js
+++ app/layout.js
@@ -2,20 +2,22 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Kannami League",
-  description: "Created by W.Segura",
+	title: "Kannami League",
+	description: "Created by W.Segura",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Theme appearance="dark">{children}</Theme>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Theme appearance="dark">{children}</Theme>
+				<Analytics />
+			</body>
+		</html>
+	);
 }
