9d16541b9ce58f5debb680d7eba200ffb434fac1
wladimirsegura <rebeldes96@gmail.com>
2024-08-17 21:16:32 +0900

    modified .env and mongoose.js to .env.local

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.local.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
