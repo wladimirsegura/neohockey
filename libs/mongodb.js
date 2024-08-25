ed8baeb5915be5285d6ee53cb7c74fece88a338f
wladimirsegura <rebeldes96@gmail.com>
2024-08-25 18:16:36 +0900

    setup MongoDB

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,12 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
-    console.log(process.env.MONGO_URI);
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    console.log(process.env.MONGO_URI);
   }
 };
 
