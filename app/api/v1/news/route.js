33bbded65af082ffa9c2b3cadfec17ee5c58802b
wladimirsegura <rebeldes96@gmail.com>
2024-10-15 04:02:45 +0900

    trying to fix module not found

--- app/api/v1/news/route.js
+++ app/api/v1/news/route.js
@@ -1,11 +1,11 @@
 import db from "/lib/db";
-import News from "/models/News";
+import news from "/models/news";
 import { NextResponse } from "next/server";
 
 export async function GET() {
 	try {
 		await db.connect();
-		const news = await News.find();
+		const news = await news.find();
 		return NextResponse.json(news);
 	} catch (error) {
 		throw new Error("DB connection error" + error);
