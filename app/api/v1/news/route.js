ebf040954bbc9b265da177221ca8e9c032906522
wladimirsegura <rebeldes96@gmail.com>
2024-10-15 22:10:20 +0900

    repared api naming

--- app/api/v1/news/route.js
+++ app/api/v1/news/route.js
@@ -1,12 +1,12 @@
 import db from "/lib/db";
-import news from "/models/news";
+import news from "@/models/news";
 import { NextResponse } from "next/server";
 
 export async function GET() {
 	try {
-		await db.connect();
-		const news = await news.find();
-		return NextResponse.json(news);
+		db.connect();
+		const newsFind = await news.find();
+		return NextResponse.json(newsFind);
 	} catch (error) {
 		throw new Error("DB connection error" + error);
 	}
